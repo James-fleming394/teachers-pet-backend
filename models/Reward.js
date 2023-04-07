3bb2116f936d037fb36d302054549bb8e174ea8b
james-fleming394 <james.fleming394@gmail.com>
2023-04-06 23:40:24 -0400

    Reward model and schema

---
+++ models/Reward.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const RewardSchema = new Schema(
+    {
+        subject: { type: String, required: true },
+        plan: { type: String, required: true },
+        img: { type: String, required: true },
+        date: { type: String, required: true },
+    }
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("Reward": RewardSchema);
