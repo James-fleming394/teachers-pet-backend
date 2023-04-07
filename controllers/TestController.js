8d546805d7c68d70fbd74892d14cce211dd0312c
james-fleming394 <james.fleming394@gmail.com>
2023-04-06 23:52:52 -0400

    Test Controller

---
+++ controllers/TestController.js
@@ -0,0 +1,72 @@
+const Test = require('../models/User');
+
+//Test
+
+const getAllTests = async (req, res) => {
+    try {
+        const allTests = await Test.find()
+        
+        res.json({ allTests })
+
+    } catch (err) {
+        res.status(500).json(err);
+
+    }
+}
+
+const getTestById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const toy = await Test.findById(id)
+    if (test) {
+        return res.status(200).json({ test })
+    }
+    return res.status(404).send('No Test Found!')
+    } catch (error) {
+    return res.status(500).send(error.message)
+    }
+}
+
+const createTest = async (req, res) => {
+    try {
+        const users = await Test.create(req.body)
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+const updateTest = async (req, res) => {
+    try {
+        const test = await Test.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    )
+        return res.status(200).json(test)
+
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+}
+
+const deleteTest = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deletedTest = await Test.findByIdAndDelete(id)
+        if (deletedTest) {
+            return res.status(200).send('Test deleted')
+        }
+        throw new Error('Test not found')
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+module.exports = {
+    getAllTests,
+    getTestById
+    createTest,
+    updateTest,
+    deleteTest
+}
